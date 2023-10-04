8b5e2ca5c8113baa828083315d4dbeb8f063f525
Tushar Dahiya <tusharxoxoxo@gmail.com>
2023-10-04 16:01:50 +0530

    dumping a piece of useless code to the corparate companies side company github

---
+++ react/first-step/src/App.tsx
@@ -0,0 +1,7 @@
+import ListGroup from "./components/ListGroup";
+//import Message from "./Message";
+function App(){
+    return <div><ListGroup></ListGroup></div>;
+    }
+
+    export default App;
---
+++ react/first-step/src/Message.tsx
@@ -0,0 +1,9 @@
+function Message(){
+    //this is not html, this is javascript XML
+    const name = "Mahadev";
+    if(name)
+        return <h1>Hello, this is Tushar, {name} i am stuck in my terminal please save me</h1>;
+    else <h1>something is really wrong here, hey Tushar take a quick look and fix this</h1>
+}
+
+export default Message;
